20ec0ea9c722d9373b2e59b1efa38a0a08e52454
ajpalok <palok1969@gmail.com>
2022-06-30 23:16:17 +0600

    hudai

--- assets/js/all_posts.js
+++ assets/js/all_posts.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 
 	/* Infinite scroll throttle and functions variables*/
 	var scrollTolerance = 900; /* Define range of pixels between scroll position and bottom page as a tolerance to trigger the function */
-	var elementsToAppendPerScroll = 3; /* Define the elements to index when user reach bottom of the site */
+	var elementsToAppendPerScroll = 5; /* Define the elements to index when user reach bottom of the site */
 
 	$.getJSON( "/assets/json/all_posts.json", function(data) {
 	/*	console.log("json data: " + data.signatories.length); */
